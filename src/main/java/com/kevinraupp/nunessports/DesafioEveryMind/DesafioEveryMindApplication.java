ecb63b7f99be68f7171f66f85e92e9f70c2428e3
oKevinRaupp <rauppke@gmail.com>
2024-01-09 10:51:01 -0300

    first commit

---
+++ src/main/java/com/kevinraupp/nunessports/DesafioEveryMind/DesafioEveryMindApplication.java
@@ -0,0 +1,13 @@
+package com.kevinraupp.nunessports.DesafioEveryMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEveryMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEveryMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinraupp/nunessports/DesafioEveryMind/DesafioEveryMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinraupp.nunessports.DesafioEveryMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEveryMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
